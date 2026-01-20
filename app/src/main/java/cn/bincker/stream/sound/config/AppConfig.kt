8aac3d85b777a359188d278f2548725a751baffb
bit16 <bit16@qq.com>
2026-01-20 17:53:35 +0800

    添加音频捕获配置

--- app/src/main/java/cn/bincker/stream/sound/config/AppConfig.kt
+++ app/src/main/java/cn/bincker/stream/sound/config/AppConfig.kt
@@ -3,5 +3,10 @@ package cn.bincker.stream.sound.config
 data class AppConfig(
     var port: Int = 12345,
     var privateKey: String = "",
-    var devices: List<DeviceConfig> = emptyList()
+    var devices: List<DeviceConfig> = emptyList(),
+    var sampleRate: Int = 48000,
+    var bits: Int = 16,
+    var channels: Int = 2,
+    var format: Int = 1,
+    var bufferSize: Int = 4096
 )
